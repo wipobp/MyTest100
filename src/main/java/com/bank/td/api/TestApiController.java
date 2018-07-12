afa16eebf68e25f6abe9ae41c401da812342b73e
wipobp <singhalok392003@gmail.com>
2018-07-12 09:28:30 +0000

    First Version

---
+++ src/main/java/com/bank/td/api/TestApiController.java
@@ -0,0 +1,11 @@
+package com.bank.td.api;
+
+import org.springframework.stereotype.Controller;
+
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2018-07-12T09:28:29.517Z")
+
+@Controller
+public class TestApiController implements TestApi {
+
+}
